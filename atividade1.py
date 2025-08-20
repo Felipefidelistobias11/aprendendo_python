6bcf547cd3fd6275bcf5a4cb4cf8c3408e8cb1b8
Felipe Fidelis Tobias <felipefidelistobias11@gmail.com>
2025-08-20 12:58:49 -0300

    utilização de variáveis e comando de exibição

---
+++ atividade1.py
@@ -0,0 +1,15 @@
+# variáveis
+nome = "Felipe"
+idade = 16
+altura = 1.67
+situacaotrabalho = False
+
+# mostrando dados
+
+print ("Olá mundo!")
+print ("Soma =", 2+3)
+print ("subtração =", 5-6)
+print ("Multiplicação =", 5*5)
+print ("Divisão =", 10/2)
+print (f"Me chamo {nome}, tenho {idade} anos, tenho {altura} de altura e minha situação de trabalho é {situacaotrabalho}")
+print ('Meu nome é', nome)
